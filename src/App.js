874756ac8bff1d5d4bc2c4e8978c002f383616ec
chris6661 <74381468+chris6661@users.noreply.github.com>
2021-03-20 22:58:30 -0500

    added footer

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import Skills from './components/Skills';
 import Experience from './components/Experience'; 
 import Portfolio from './components/Portfolio'; 
 import Contact from './components/Contact'; 
+import Footer from './components/Footer'; 
 
 function App() {
   return (
@@ -39,7 +40,8 @@ function App() {
     <Skills />
     <Experience />
     <Portfolio />
-    <Contact/>
+    <Contact />
+    <Footer />
     </>
   );
 }
